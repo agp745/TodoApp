88ed1b1c148ad8fb9bd426ceac199d08ea5bda8a
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-07-25 20:37:08 -0500

    server and client containerized

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,24 @@
-
+import { useEffect, useState } from "react"
 
 export default function App() {
+
+  const serverURL = 'http://localhost:8080'
+
+  const [json, setJson] = useState('recieving json')
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const res = await fetch(`${serverURL}`)
+    console.log(res)
+  }
+
   return (
     <>
-      <div>frontend</div>    
+      <div>frontend</div>  
+      <p>does reload work?</p>  
     </>
   )
 }
